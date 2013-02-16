bc0dce8bca240f3cbcbd2dbd0a061b59db26cec6
VienosNotes <scarletwine_vieno@yahoo.co.jp>
2013-02-16 21:44:38 +0900

    ほげ

--- 1-1/search.php
+++ 1-1/search.php
@@ -2,20 +2,18 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title></title>
-  </head><body>
-    <?php
-    $fileName = "target.txt";
-    $file = fopen($fileName, "r");
- 
-    while (!feof($file)) {
-      $str = fgets($file);
-      if (preg_match($_GET['query'], $str) == 1) {
-          print($str);
-      }
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+     <title></title>
+     </head><body>
+     <?php $fileName = "target.txt";
+     $file = fopen($fileName, "r");
+print("hoge");
+while (!feof($file)) {
+    $str = fgets($file);
+    if (preg_match($_GET['query'], $str) == 1) {
+        print($str);
     }
- 
+}
     fclose($file);
     ?>
   </body>
