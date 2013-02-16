e6896fb297af8370eeccdfeb8181402c6c7b943f
VienosNotes <scarletwine_vieno@yahoo.co.jp>
2013-02-16 20:53:15 +0900

    add index.php

---
+++ 1-1/index.php
@@ -0,0 +1,64 @@
+<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html lang="JA">
+
+<head>
+ <meta http-equiv="Content-Type" content="text/html;charset=ISO-2022-JP"/>
+ <title>メディア情報検索の基礎 課題1-1 sample</title>
+</head>
+
+<body bgcolor="white" text="black">
+
+<?php phpinfo(); ?>
+<h1 align="center">メディア情報検索の基礎　課題1-1</h1>
+<hr align="center" noshade width="80%"/>
+<div align="center">
+
+<form method="post" action="./sample.cgi">
+<input type="text" name="KEYWORD" size="15"/>
+<input type="submit" value="submit"/>
+</form>
+
+</div>
+
+<hr align="center" noshade width="80%"/>
+
+<table border="1" align="center">
+
+<caption>検索対象データ</caption>
+
+<tr>
+ <th>ID</th><th>title</th><th>author</th>
+</tr>
+<tr>
+ <td><a href="http://www.louvre.fr/francais/magazine/peint/joconde/jocon_f.htm">Image1</a></td>
+ <td>Les visages de la Joconde</td><td>Leonardo di ser Piero da Vinci</td>
+</tr>
+<tr>
+ <td><a href="http://www.moma.org/docs/collection/paintsculpt/c58.htm">Image2</a></td>
+ <td>The Starry Night</td><td>Vincent van Gogh</td>
+</tr>
+<tr>
+ <td><a href="http://www.slam.org/am1.html">Image3</a></td>
+ <td>The Jolly Flatboatmen in Port</td><td>George Caleb Bingham</td>
+</tr>
+<tr>
+ <td><a href="http://national.gallery.ca/slidekits/slidekit_escher/sk_escher10.html">Image4</a></td>
+ <td>Waterfall 1961</td><td>M.C. Escher</td>
+</tr>
+<tr>
+ <td><a href="http://www.monetalia.com/paintings/monet-garden-of-the-princess-louvre.aspx">Image5</a></td>
+ <td>Garden of the Princess, Louvre</td><td>Claude Monet</td>
+</tr>
+
+</table>
+
+<hr align="center" noshade width="80%"/>
+
+<div align="center">
+<a href="../text.html">戻る</a>
+</div>
+
+</body>
+
+</html>
