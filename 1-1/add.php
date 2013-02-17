a974902969698a2317efd2a2d88633d4a76df555
VienosNotes <scarletwine_vieno@yahoo.co.jp>
2013-02-17 14:58:06 +0900

    fix path

--- 1-1/add.php
+++ 1-1/add.php
@@ -3,5 +3,5 @@ $fh = fopen("target.txt", "a");
 fwrite($fh, htmlspecialchars($_POST['content']));
 fclose($fh);
   header("HTTP/1.1 301 Moved Permanently");
-  header("Location: http://example.jp/");
+  header("Location: ./index.html");
 ?>
